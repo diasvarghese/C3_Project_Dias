1194cec146c1f369c9f001f6a0d820731a2f8169
Dias Varghese <76867017+diasvarghese@users.noreply.github.com>
2021-03-01 20:35:18 +0400

    Initial commit

---
+++ Restaurant_project -Student version/src/test/java/RestaurantTest.java
@@ -0,0 +1,84 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import java.time.LocalTime;
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+
+class RestaurantTest {
+    Restaurant restaurant;
+
+    //REFACTOR ALL THE REPEATED LINES OF CODE
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
+    @Test
+    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
+                //WRITE UNIT TEST CASE HERE
+        RestaurantService newRest = new RestaurantService();
+        restaurant = newRest.addRestaurant("CCD","MG Road",LocalTime.parse("08:00:00"),LocalTime.parse("22:00:00"));
+
+        LocalTime currentTime = LocalTime.parse("09:00:00");
+        Assertions.assertTrue(currentTime.isAfter(restaurant.openingTime));
+        Assertions.assertTrue(currentTime.isBefore(restaurant.closingTime));
+
+    }
+
+    @Test
+    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
+        //WRITE UNIT TEST CASE HERE
+        RestaurantService newRest = new RestaurantService();
+        restaurant = newRest.addRestaurant("CCD","MG Road",LocalTime.parse("08:00:00"),LocalTime.parse("20:00:00"));
+
+        LocalTime currentTime = LocalTime.parse("04:00:00");
+        LocalTime currentTime2 = LocalTime.parse("23:30:00");
+
+        Assertions.assertFalse(currentTime.isAfter(restaurant.openingTime));
+        Assertions.assertFalse(currentTime2.isBefore(restaurant.closingTime));
+        }
+
+    //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void adding_item_to_menu_should_increase_menu_size_by_1(){
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.addToMenu("Sizzling brownie",319);
+        assertEquals(initialMenuSize+1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.removeFromMenu("Vegetable lasagne");
+        assertEquals(initialMenuSize-1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_that_does_not_exist_should_throw_exception() {
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+
+        assertThrows(itemNotFoundException.class,
+                ()->restaurant.removeFromMenu("French fries"));
+    }
+    //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+}
