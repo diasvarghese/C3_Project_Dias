618aef86d0bf36fe9c14476ed8739b7c584994e4
Dias Varghese <76867017+diasvarghese@users.noreply.github.com>
2021-03-01 21:25:12 +0400

    Part 2: Solution
    All methods implemented
    All test cases pass

---
+++ RestaurantService.java
@@ -0,0 +1,45 @@
+//import org.graalvm.compiler.debug.Assertions;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+
+    public RestaurantService(){
+
+        restaurants.add(new Restaurant("Pizza Hut","Brigade Road",LocalTime.parse("10:00:00"),LocalTime.parse("22:00:00")));
+
+    }
+
+
+    public Restaurant findRestaurantByName(String restaurantName) {
+
+        for (Restaurant restaurant : restaurants) {
+            if (restaurant.getName().equals(restaurantName)) {
+                return restaurant;
+            }
+        }
+            return null;
+        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+    }
+
+
+
+    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        restaurants.add(newRestaurant);
+        return newRestaurant;
+    }
+
+    public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
+        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+        restaurants.remove(restaurantToBeRemoved);
+        return restaurantToBeRemoved;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+}
